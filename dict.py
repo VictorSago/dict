d82091444e604d76f27747dd6f29a8889dfdbb74
Victor Sago <VictorSago01@gmail.com>
2022-06-09 15:41:52 +0200

    Added some explanatory comments

--- dict.py
+++ dict.py
@@ -6,20 +6,34 @@ conn = psycopg2.connect(
    password="abc123"
 )
 
+# read_dict: returns the list of all dictionary entries:
+#   argument: C - the database connection
 def read_dict(C):
     cur = C.cursor()
     cur.execute("SELECT id, word, translation FROM dictionary;")
     rows = cur.fetchall()
     cur.close()
     return rows
+
+# add_word: adds a word and its translation to the list
+#   argument: C - the database connection
+#   argument: word - the English word to be added to the listz
+#   argument: translation - translation of the word into Swedish
 def add_word(C, word, translation):
     cur = C.cursor()
     cur.execute(f"INSERT INTO dictionary (word, translation) VALUES ('{word}', '{translation}');")
     cur.close()
+
+# delete_word: removes a word from the list
+#   argument: C - the database connection
+#   argument: ID - the database ID of the word to be removed 
 def delete_word(C, ID):
     cur = C.cursor()
     cur.execute(f"DELETE FROM dictionary WHERE id = '{ID}';")
     cur.close()
+
+# save_dict: commits all changes to the database
+#   argument: C - the database connection
 def save_dict(C):
     cur = C.cursor()
     cur.execute("COMMIT;")
